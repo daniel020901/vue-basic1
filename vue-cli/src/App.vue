2f767fedcb78cdc16f143b1d2668f5eb19a0c05f
daniel020901 <muhammaddanielilyasa@gmail.com>
2024-03-11 10:55:58 +0700

    pertama

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app" class="container mt-5">
+    <h1>IDshop</h1>
+    <price-slider :sliderStatus="sliderStatus" :maximum.sync="maximum"></price-slider>
+    <product-list :products="products" :maximum="maximum" @add="addItem"></product-list>
+    
+  </div>
+</template>
+
+<script>
+// import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import PriceSlider from "./components/PriceSlider.vue";
+import ProductList from "./components/ProductList.vue";
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      maximum: 50,
+      products: [],
+      cart: [],
+      sliderStatus: true
+    }
+  },
+  components: {
+    // FontAwesomeIcon,
+    PriceSlider,
+    ProductList
+  },
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+  methods: {
+    addItem: function (product) {
+      let productIndex;
+      let productExist = this.cart.filter(function (item, index) {
+        if (item.product.id == Number(product.id)) {
+          productIndex = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      if (productExist.length) {
+        this.cart[productIndex].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    },
+  }
+};
+</script>
+
---
+++ vue-cli/src/components/PriceSlider.vue
@@ -0,0 +1,44 @@
+<template>
+     <transition name="custom" enter-active-class="animated fadeInDown" leave-active-class="animated slideOutRight">
+            <div v-if="sliderStatus">
+                <div class="align-items-center" :class="sliderState">
+                    <label for="" class="font-weight-bold-mr-2">Max</label>
+                    <input type="text" class="form-control mx-2"
+                        style="width: 60px; text-align: Center;" v-model="maxAmount" @change="$emit('update:maximum', maxAmount)">
+                    <input type="range" clas="custom-range" min="0" max="200" v-model="maxAmount" @input="$emit('update:maximum', maxAmount)">
+                </div>
+            </div>
+        </transition>
+</template>
+
+<script>
+export default {
+    name:"slider-status",
+    data: function() {
+        return {
+            maxAmount: 50
+        }
+    },
+    props:["sliderStatus", "maximum"],
+    computed: {
+        sliderState: function () {
+      return this.style.sliderStatus ? "d-flex" : "d-none";
+    },
+    }
+}
+</script>
+
+<style>
+.navbar{
+    background-color: transparent;
+}
+.fade-enter, .fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: all 3s ease-in-out;
+}
+
+
+</style>
